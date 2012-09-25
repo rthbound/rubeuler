ce66859278b1ccde34f01a4a8884ce8c73fb5837
Tad Hosford <tad@isotope11.com>
2012-09-24 22:20:33 -0500

    Remove unused dependencies

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -9,11 +9,5 @@ require "pry"
 # The gem
 $: << File.dirname(__FILE__) + "/../lib"
 $: << File.dirname(__FILE__)
-require "rubeuler"
 
-# Turn configuration
-#Turn.config do |config|
-#  config.format = :pretty
-#  # config.trace = true
-#  config.natural = true
-#end
+require "rubeuler"
